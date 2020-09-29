dd69929525cf3b1494752a5bea97f68497da47d9
Your NameIrina-S <serysheva.i@yandex.ru>
2020-09-30 02:15:03 +0900

    Исправлены данные для подлючения к бд

--- application/config/db.php
+++ application/config/db.php
@@ -6,10 +6,3 @@ return [
     'user' => 'root',
     'password' => '' 
 ];
-
-// return [
-//     'host' => 'localhost',
-//     'dbname' => '9243757924_kodarcenter',
-//     'user' => '9243757924',
-//     'password' => 'jino8914482' 
-// ];
